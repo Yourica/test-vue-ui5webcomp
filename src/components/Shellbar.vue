02a90b330231844cef36b38b01661859e4c4b744
Yourica <youri.catinat@gmail.com>
2022-05-14 20:24:18 +0200

    Change 14/05

--- src/components/Shellbar.vue
+++ src/components/Shellbar.vue
@@ -8,4 +8,5 @@ import "@ui5/webcomponents-icons/dist/AllIcons";
 	<ui5-shellbar primary-title="WTC3 Charon" secondary-title="Secondary title">
 		<ui5-avatar shape="Square" icon="customer"></ui5-avatar>
 	</ui5-shellbar>
+	Test !!
 </template>
